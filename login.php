5f5c71c5d0d07467ac60718f522ea899fe850e91
khushigupta124 <149676414+khushigupta124@users.noreply.github.com>
2025-07-18 22:35:16 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
     $username = $_POST['username'];
     $password = $_POST['password'];
     
@@ -17,7 +18,9 @@
         if($stmt_result->num_rows > 0) {
             $data=$stmt_result->fetch_assoc();
             if($data['password'] === $password){
-                echo "<h2>Login successfully</h2>";
+                $_SESSION['username'] = $username;
+                header('Location: final_homepage.html');
+                exit();
             }
             else{
                 echo "<h2>Invalid Email or password</h2>";
